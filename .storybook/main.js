ca2e7c47e689c25b1310e6cc0ddc55b237c1d014
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-21 16:02:17 -0300

    chore(storybook): config storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,36 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app"
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5"
+  },
+  webpackFinal: (config) => {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: 'node_modules/@gmcotta/design-tokens/dist/css/global.css',
+            to: 'tokens/global.css'
+          },
+          {
+            from: '**/**/*.css',
+            context: 'node_modules/@gmcotta/design-tokens/dist/css',
+            to: 'tokens/[path]/[name][ext]'
+          }
+        ]
+      })
+    );
+    return config;
   }
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,3 +7,68 @@ export const parameters = {
     },
   },
 }
+
+export const globalTypes = {
+  theme: {
+    name: 'Themes',
+    description: 'Temas',
+    defaultValue: 'marca-a/tema-1',
+    right: true,
+    toolbar: {
+      icon: 'globe',
+      items: [
+        {
+          value: 'marca-a/tema-1',
+          title: 'Marca A',
+          right: 'Tema 1'
+        },
+        {
+          value: 'marca-a/tema-2',
+          title: 'Marca A',
+          right: 'Tema 2'
+        }
+      ],
+      showName: true
+    }
+  },
+  mode: {
+    name: 'Mode',
+    description: 'Mode',
+    defaultValue: 'light',
+     right: true,
+     toolbar: {
+       icon: 'globe',
+       items: [
+         {
+          value: 'light',
+          title: 'Light',
+         },
+         {
+          value: 'dark',
+          title: 'Dark',
+         }
+       ],
+       showName: true
+     }
+  }
+}
+
+export const decorators = [
+  (Story, context) => {
+    let style = '';
+    let [brand, theme] = context.globals.theme.split('/');
+    let mode = context.globals.mode;
+    if (mode === 'dark') {
+      style = `.sb-show-main { background: #292929 !important; }`;
+    }
+
+    return html`
+      <style>${style}</style>
+      <div brand=${brand} theme=${theme} mode=${mode}>
+      <link rel="stylesheet" type="text/css" href="tokens/global.css" />
+      <link rel="stylesheet" type="text/css" href="tokens/${brand}/${theme}/${mode}.css" />
+        ${Story()}
+      </div>
+    `;
+  }
+]
